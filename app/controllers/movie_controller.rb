cf42d3e6498d110b109f4d3d4b8ed1bb03d7e39e
Aditi Green <asaki444@gmail.com>
2017-06-11 04:46:36 +0000

    modified the controller to include edit, removing a movie from a user profile

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -3,8 +3,19 @@ class MoviesController < ApplicationController
     @user = User.find(params[:id])
     erb :'/movies/choosemovie'
   end
-
+  get '/movies/:id/edit' do
+    @user = User.find(session[:id])
+    erb :'/movies/edit'
+  end
   post '/movies/:id/choosemovie' do
+    @user = User.find(params[:id])
+    @movies = Movie.find(params["movie_ids"])
+    @user.movies = @movies
+    @user.save
+    redirect "/users/#{@user.id}/show"
+  end
+
+  post 'movies/:id/edit' do
     @user = User.find(params[:id])
     @movies = Movie.find(params["movie_ids"])
     @user.movies = @movies
